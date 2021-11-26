d968f38ab788f8e7602eff4cfa0ae4436c0607d7
刁斤 平 <minglu.mit@gmail.com>
2021-11-26 14:55:51 +0000

    1

---
+++ Project: Simple Chatty Bot/Work on project. Stage 1 Chatty Bot welcomes you.py
@@ -0,0 +1,32 @@
+'''
+Description
+Digital personal assistants help people to drive cars, plan their day, buy something online. In a sense, they are simplified versions of artificial intelligence with whom you can talk.
+
+In this project, you will develop step by step a simple bot that will help you study programming.
+
+Objective
+For the first stage, you will write a bot who displays a greeting, its name, and the date of its creation. First impressions count!
+
+Your program should print the following lines:
+
+Hello! My name is {bot_name}.
+I was created in {birth_year}.
+Instead of {bot_name}, print any name you choose and replace {birth_year} with the current year (four digits).
+
+Example
+Output:
+
+Hello! My name is Aid.
+I was created in 2020.
+You can change the text if you want but print exactly two lines.
+
+Next, we will use Aid and 2020 as your bot's name and its birth year, but you can change it if you need to.
+
+'''
+
+# write your code here
+birth_year = 2020
+bot_name = 'Aid'
+
+print(f'Hello! My name is {bot_name}.')
+print(f'I was created in {birth_year}.')
