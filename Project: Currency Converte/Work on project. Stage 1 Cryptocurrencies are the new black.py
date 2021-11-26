8e9365a1551349d41a0a482762387b68da654606
刁斤 平 <minglu.mit@gmail.com>
2021-11-26 16:11:50 +0000

    1

---
+++ Project: Currency Converte/Work on project. Stage 1 Cryptocurrencies are the new black.py
@@ -0,0 +1,16 @@
+'''
+Description
+Today we start our new project. It will be a simple currency converter. Every person sometimes needs to convert one currency to another. But we need to start easy, so for now, all you need to do is to print "Meet a conicoin!" Please, make sure that the output formatting of your program follows the example output formatting.
+
+Objectives
+Imagine that there is a cryptocurrency called conicoin ("coni" is just an anagram of the word "coin"). Greet conicoin as shown in the example below.
+
+Example
+Output:
+
+Meet a conicoin!
+
+'''
+
+# write your code here!
+print('Meet a conicoin!')
