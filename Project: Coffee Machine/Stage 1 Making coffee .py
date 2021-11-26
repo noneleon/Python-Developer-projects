f5550111b55410b00beacf3fba932b957cad0fa4
刁斤 平 <minglu.mit@gmail.com>
2021-11-26 15:44:45 +0000

    1

---
+++ Project: Coffee Machine/Stage 1 Making coffee .py
@@ -0,0 +1,34 @@
+'''
+Description
+Let's start with a program that makes you a coffee – virtual coffee, of course. In this project, you will implement functionality that simulates a real coffee machine. It can run out of ingredients, such as milk or coffee beans, it can offer you various types of coffee, and, finally, it will take money for the prepared drink.
+
+Objective
+The first version of the program just makes you a coffee. It should print to the standard output what it is doing as it makes the drink.
+
+Example
+Take a look at the sample output below and print all the following lines.
+
+Output:
+
+Starting to make a coffee
+Grinding coffee beans
+Boiling water
+Mixing boiled water with crushed coffee beans
+Pouring coffee into the cup
+Pouring some milk into the cup
+Coffee is ready!
+'''
+
+# Write your code here
+text = """
+Starting to make a coffee
+Grinding coffee beans
+Boiling water
+Mixing boiled water with crushed coffee beans
+Pouring coffee into the cup
+Pouring some milk into the cup
+Coffee is ready!
+"""
+print(text)
+
+
